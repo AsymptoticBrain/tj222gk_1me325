e9b1a2eac4b3388f907055423a5c4f9f3c4e41b4
Timothy Johansson <Tim@Timothys-Air.lan>
2019-02-27 13:11:35 +0100

    Added comments

--- Project/src/js/Dice.js
+++ Project/src/js/Dice.js
@@ -8,6 +8,12 @@
 // Public class
 //--------------------------------------------------------------------------
 
+/**
+ * Creates the dice object and adds the methods to create, destroy and cast the
+ * die. Accepts a parent parameter to determine where to append the object.
+ * 
+ * @param {*} parent 
+ */
 function Dice (parent) {
 
     this.dieParent = parent;
@@ -28,6 +34,11 @@ Dice.prototype = {
 
     constructor : Dice, 
 
+    /**
+     * Creates the actual dice object with a random face value which determines the class
+     * of the div object. Uses NewElem class to return a useable die that is appended to 
+     * its parentclass. Also adds eventlistener to allow the die to be cast when clicked.
+     */
     dieCreate : function() {
 
         var that = this;
@@ -38,6 +49,10 @@ Dice.prototype = {
 
     },
 
+    /**
+     * Simulates a random die cast, if the die value has already been set it updates the 
+     * die class atribute so the face changes to the correct value.
+     */
     dieCast : function() {
 
         var faceArray = ["one", "two", "three", "four", "five", "six"];
@@ -53,6 +68,9 @@ Dice.prototype = {
 
     }, 
 
+    /**
+     * Removes the die from the parent element.
+     */
     dieDestroy : function() {
 
         this.dieParent.removeChild(this.die); 
